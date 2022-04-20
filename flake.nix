363aa852f4d90534ac636ffd85700cc3e18225d6
Matthew Stewart <m.scott.stewart@gmail.com>
2022-04-20 01:49:31 -0400

    Initial commit of flake.nix and home.nix.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My Nix Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      username = "rotaerk";
+    in {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        configuration = import ./home.nix;
+
+        inherit system username;
+        homeDirectory = "/home/${username}";
+
+        stateVersion = "21.11";
+      };
+    };
+}
